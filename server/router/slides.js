5100ac3895ee3203b438c7a0237e2014bd7e5cbd
George Varghese M <george@georgevm.com>
2021-03-12 01:08:08 +0530

    Add update route for slides

--- server/router/slides.js
+++ server/router/slides.js
@@ -3,23 +3,91 @@ const { createHash } = require('crypto');
 const { Router } = require('express');
 const withError = require('./with-error');
 const { serverUrl } = require('../config');
+const { Op } = require('sequelize');
 
 const notFound = res => res.status(400).send({ message: 'Slide not found' });
 
+const duplicate = res =>
+  res.status(409).json({ message: 'A slide with that name already exists' });
+
+const slideExists = async (name, id) => {
+  const where = { name };
+
+  if (id) {
+    where.id = { [Op.not]: id };
+  }
+
+  const count = await models.slide.count({ where });
+  return count !== 0;
+};
+
+const decodeData = data => {
+  const buffer = Buffer.from(data, 'base64');
+  const etag = createHash('md5')
+    .update(buffer)
+    .digest()
+    .toString('hex')
+    .toUpperCase();
+
+  return { buffer, etag };
+};
+
 const upload = async (req, res) => {
   const { name, data, type } = req.body;
-  const dataBuffer = Buffer.from(data, 'base64');
-  const etag = createHash('md5').update(dataBuffer).digest();
+  const { buffer, etag } = decodeData(data);
+
+  if (await slideExists(name)) {
+    return duplicate(res);
+  }
 
   const slide = await models.slide.create({
     name,
     type,
-    data: dataBuffer,
-    etag: etag.toString('hex').toUpperCase(),
+    data: buffer,
+    etag,
   });
 
   return res.status(200).json({
     id: slide.id,
+    type: slide.type,
+    name: slide.name,
+    etag: slide.etag,
+  });
+};
+
+const update = async (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name, data, type } = req.body;
+
+  const slide = await models.slide.findOne({ where: { id } });
+
+  if (!slide) {
+    return notFound(res);
+  }
+
+  if (name) {
+    if (await slideExists(name, id)) {
+      return duplicate(res);
+    }
+
+    slide.name = name;
+  }
+
+  if (data) {
+    const { buffer, etag } = decodeData(data);
+    slide.data = buffer;
+    slide.etag = etag;
+  }
+
+  if (type) {
+    slide.type = type;
+  }
+
+  await slide.save();
+
+  return res.json({
+    id: slide.id,
+    type: slide.type,
     name: slide.name,
     etag: slide.etag,
   });
@@ -31,6 +99,7 @@ const list = async (req, res) => {
     slides: slides.map(slide => ({
       id: slide.id,
       name: slide.name,
+      type: slide.type,
       etag: slide.etag,
       url: `${serverUrl}/api/slides/${slide.id}`,
     })),
@@ -38,7 +107,7 @@ const list = async (req, res) => {
 };
 
 const get = async (req, res) => {
-  const id = req.params.id;
+  const id = parseInt(req.params.id);
 
   const attributes = ['etag', 'type'];
 
@@ -62,6 +131,7 @@ const get = async (req, res) => {
 };
 
 const deleteSlide = async (req, res) => {
+  const id = parseInt(req.params.id);
   const count = await models.slide.count({ where: { id } });
 
   if (!count) {
@@ -76,6 +146,7 @@ const router = Router();
 router.get('/', withError(list));
 router.post('/new', withError(upload));
 router.get('/:id', withError(get));
+router.post('/:id', withError(update));
 router.delete('/:id', withError(deleteSlide));
 
 module.exports = router;
