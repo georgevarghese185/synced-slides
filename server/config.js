33daa03a6c0be96d0738f934641e779652dd7287
George Varghese M <george@georgevm.com>
2021-03-11 23:33:21 +0530

    Add routes to get and delete slides

--- server/config.js
+++ server/config.js
@@ -7,6 +7,7 @@ if (process.env.NODE_ENV !== 'production') {
 module.exports = {
   env: process.env.NODE_ENV || 'production',
   port: process.env.PORT || 8080,
+  serverUrl: process.env.SERVER_URL,
   db: {
     dialect: 'postgres',
     host: process.env.DB_HOST,
@@ -14,6 +15,6 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    logging: process.env.DB_LOGS === 'true',
+    logging: process.env.DB_LOGS === 'true' && console.log,
   },
 };
--- server/router/slides.js
+++ server/router/slides.js
@@ -2,6 +2,9 @@ const db = require('../sequelize');
 const { createHash } = require('crypto');
 const { Router } = require('express');
 const withError = require('./with-error');
+const { serverUrl } = require('../config');
+
+const notFound = res => res.status(400).send({ message: 'Slide not found' });
 
 const upload = async (req, res) => {
   const { name, data, type } = req.body;
@@ -22,7 +25,58 @@ const upload = async (req, res) => {
   });
 };
 
+const list = async (req, res) => {
+  const slides = await db.slides.findAll();
+  res.json({
+    slides: slides.map(slide => ({
+      id: slide.id,
+      name: slide.name,
+      etag: slide.etag,
+      url: `${serverUrl}/api/slides/${slide.id}`,
+    })),
+  });
+};
+
+const get = async (req, res) => {
+  const id = req.params.id;
+
+  const attributes = ['etag', 'type'];
+
+  if (req.method === 'GET') {
+    attributes.push('data');
+  }
+
+  const slide = await db.slides.findOne({
+    where: { id },
+    attributes,
+  });
+
+  if (!slide) {
+    return notFound(res);
+  }
+
+  res.set('Content-Type', slide.type);
+  res.set('ETag', `W/"${slide.etag}"`);
+  res.set('Cache-Control', 'must-revalidate');
+  res.send(slide.data);
+};
+
+const deleteSlide = async (req, res) => {
+  const id = req.params.id;
+  const count = await db.slides.count({ where: { id } });
+
+  if (!count) {
+    return notFound(res);
+  }
+
+  await db.slides.destroy({ where: { id } });
+  res.json({ message: 'Deleted' });
+};
+
 const router = Router();
+router.get('/', withError(list));
 router.post('/new', withError(upload));
+router.get('/:id', withError(get));
+router.delete('/:id', withError(deleteSlide));
 
 module.exports = router;
