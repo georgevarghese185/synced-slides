4ddc3b749dc8d8c47f8eab658c340a82473f5ef8
George Varghese M <george@georgevm.com>
2021-03-20 18:57:23 +0530

    Add scripts to start frontend and backend together

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -46,7 +46,8 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
+      distDir: '../dist',
 
       // transpile: false,
 
