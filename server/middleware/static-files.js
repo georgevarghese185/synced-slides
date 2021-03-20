f07819cc21f66f9337a113f4a20ed099f244cc48
George Varghese M <george@georgevm.com>
2021-03-20 16:38:50 +0530

    Add redirects for admin and display pages

--- server/middleware/static-files.js
+++ server/middleware/static-files.js
@@ -3,6 +3,12 @@ const express = require('express');
 const staticMiddleware = express.static('./dist');
 
 module.exports = (req, res, next) => {
+  if (req.auth.isAdmin && !req.url.startsWith('/admin')) {
+    return res.redirect('/admin');
+  } else if (!req.auth.isAdmin && !req.url.startsWith('/display')) {
+    return res.redirect('/display');
+  }
+
   staticMiddleware(req, res, () => {
     req.url = '/';
     staticMiddleware(req, res, next);
