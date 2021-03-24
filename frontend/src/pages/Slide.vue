16f6ed608112f792e40ae05cfc6c1327d53401a5
George Varghese M <george@georgevm.com>
2021-03-24 21:27:19 +0530

    Fix update loaders on slide page

--- frontend/src/pages/Slide.vue
+++ frontend/src/pages/Slide.vue
@@ -37,14 +37,14 @@
       <p v-if="uploadError" class="text-body1 text-red">{{uploadError}}</p>
 
       <div>
-        <q-btn label="Save" type="submit" :loading="uploadLoading" color="primary"/>
+        <q-btn label="Save" type="submit" :loading="uploadLoading || updateLoading" color="primary"/>
         <q-btn
           flat
           label="Cancel"
           color="primary"
           to="/admin/slides"
           class="q-ml-sm"
-          :disable="uploadLoading"
+          :disable="uploadLoading || updateLoading"
         />
       </div>
     </q-form>
