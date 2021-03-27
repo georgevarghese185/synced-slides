69533069e0a6d75a89b6e18d14bfe9f1313efb79
George Varghese M <george@georgevm.com>
2021-03-27 17:17:01 +0530

    Use relative path for images

--- server/util/slide.js
+++ server/util/slide.js
@@ -1,7 +1,5 @@
-const config = require('../config');
-
 const slideUrl = slide => {
-  return `${config.serverUrl}/api/slides/image/${slide.uuid}`;
+  return `/api/slides/image/${slide.uuid}`;
 };
 
 module.exports = {
