28b6a94c75a62ad2a79e1c8e2b39879aae08acca
George Varghese M <george@georgevm.com>
2021-03-29 01:03:17 +0530

    Rename display_slides model to singular

--- server/model/index.js
+++ server/model/index.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 const config = require('../config');
 const Display = require('./display');
 const Slide = require('./slide');
-const DisplaySlides = require('./display-slides');
+const DisplaySlide = require('./display-slide');
 
 const sequelize = new Sequelize({
   ...config.db,
@@ -10,13 +10,14 @@ const sequelize = new Sequelize({
 
 const slide = Slide(sequelize);
 const display = Display(sequelize);
-const displaySlides = DisplaySlides(sequelize);
+const displaySlide = DisplaySlide(sequelize);
 
-slide.belongsToMany(display, { through: displaySlides });
-display.belongsToMany(slide, { through: displaySlides });
+slide.belongsToMany(display, { through: displaySlide });
+display.belongsToMany(slide, { through: displaySlide });
 
 module.exports = {
   Slide: slide,
   Display: display,
+  DisplaySlide: displaySlide,
   sequelize,
 };
